67482bd759ff71de137c0e9f9f6299da63a9d6d3
Neal R <nealjrobison@gmail.com>
2018-07-31 14:44:08 -0400

    Modified Class Output

--- Characters/Character.rb
+++ Characters/Character.rb
@@ -1,7 +1,12 @@
 class Character
-    def initialize(name, race, str, dex, con, int, wis, cha)
+    def initialize(name, race, _class, alignment, str, dex, con, int, wis, cha)
+        @level = 1
+        @exp = 0
+        @levelEXP = {_1:300,_2:900,_3:2700,_4:6500}
         @name = name
         @race = race
+        @_class = _class
+        @alignment = alignment
         @str = str
         @dex = dex
         @con = con
@@ -11,16 +16,46 @@ class Character
     end
 
     def to_s
-        "#{@name}
-        \n#{@race}
-        \n#{@str}
-        \n#{@dex}
-        \n#{@con}
-        \n#{@int}
-        \n#{@wis}
-        \n#{@cha}"
+        "\tName: #{@name}   Class: #{@_class}(#{@level})
+        Race: #{@race}  Alignment: #{@alignment} EXP: #{@exp}
+        STR: #{@str}
+        DEX: #{@dex}
+        CON: #{@con}
+        INT: #{@int}
+        WIS: #{@wis}
+        CHA: #{@cha}"
+    end
+
+    def addLevels(num)
+        @level += num
+    end
+
+    def addEXP(num)
+        @exp += num
+        if @exp >= @levelEXP[:"_#{@level}"]
+            self.addLevels(1)
     end
 end
 
-c = Character.new('Randwulf','High Elf',15,12,10,15,12,14)
+c = Character.new('Randwulf','High Elf','Paladin','Neutral Good',15,12,10,15,12,14)
 puts c
+c.addEXP(300)
+puts c
+c.addEXP(300)
+puts c
+c.addEXP(300)
+puts c
+c.addEXP(300)
+puts c
+c.addEXP(300)
+puts c
+c.addEXP(300)
+puts c
+c.addEXP(300)
+puts c
+c.addEXP(300)
+puts c
+c.addEXP(300)
+puts c
+
+end
