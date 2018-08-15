ab7c6098c291399413a25ae6dad88ea244472402
Neal R <nealjrobison@gmail.com>
2018-08-14 20:18:48 -0400

    Major menu updates

--- Characters/Character.rb
+++ Characters/Character.rb
@@ -32,12 +32,11 @@
 
 class Character
   @@roster = {}
-  @@count = 0
   attr_accessor :name, :race, :_class, :level
   def initialize(name, race, _class, alignment, str, dex, con, int, wis, cha)
     @level = 1
     @exp = 0
-     @name = name
+    @name = name
     @race = race
     @_class = _class
     @alignment = alignment
@@ -48,8 +47,6 @@ class Character
     @wis = wis
     @cha = cha
     @@roster = @@roster.merge!("#{name}"=>self)
-    @@count+=1
-    #puts self
   end
   
   def setLevel(exp)
@@ -92,8 +89,11 @@ class Character
       @level = 18
     when(305000...355000)
       @level = 19
-    when exp >= 355000
+    when (355000..355000)
       @level = 20
+    else
+      msg = "Error setting exp/lvl information."
+      puts msg
     end
   end
 
@@ -116,75 +116,7 @@ class Character
     @@roster
   end
 
-  def self.get_levelEXP
-    @@levelEXP
-  end
-
-  def addLevels(num)
-    @level += num
-  end
-
-  def removeLevels(num)
-    @level -= num
-  end
-
-  def nextLvlEXP()
-    if @level == 1
-      0
-    elsif @level == 20
-      355_000
-    else
-      @@levelEXP[:"_#{@level + 1}"]
-    end
-  end
-
-  def prevLvlEXP()
-    if @level == 1
-      0
-    elsif @level == 20
-      355_000
-    else
-      @@levelEXP[:"_#{@level}"]
-    end
-  end
-
-  def addEXP(num)
-    if @level == 20
-      msg = 'You are already at Max Level! (20)'
-      puts msg
-    else
-      @exp += num
-      setLevel(exp)
-      # while @exp >= nextLvlEXP()
-      #   addLevels(1)
-      #   break if @level == 20
-      # end
-      puts self
-    end
-  end
-
-  def removeEXP(num)
-    if @level == 1
-      msg = 'You are already at First Level! (1)'
-      puts msg
-    else
-      @exp -= num
-      while @exp < prevLvlEXP()
-        removeLevels(1)
-        break if @level == 1
-      end
-      puts self
-    end
-  end
-
   def setEXP(num)
-    # if num > @exp
-    #   temp = num - @exp
-    #   addEXP(temp)
-    # elsif num < @exp
-    #   temp = @exp - num
-    #   removeEXP(temp)
-    # end
     @exp = num
     self.setLevel(@exp)
   end
@@ -228,26 +160,18 @@ class Character
     roster = get_roster()
     names = roster.keys()
     loop do
-      names.each do |x|
-        msg = "\n #{x}"
-        puts msg
-      end
-      msg = "\nPlease enter a character name from the list: "
-      puts msg
-      input = gets.chomp
-      if names.include?(input)
-        current = roster[input]
-        puts current
-        msg = "Please select an option: 
-        1) Character List
-        2) Edit Current
+      msg = "Please select an option: 
+        1) View Character
+        2) Edit Character
         3) Main Menu"
         puts msg
         choice = gets.to_i()
         case choice
         when 1
-          next
+          current = getCharacter()
+          puts current
         when 2
+          current = getCharacter()
           msg = "Please select an option: 
           1) Set EXP"
           puts msg
@@ -258,17 +182,39 @@ class Character
             puts msg
             exp = gets.to_i()
             current.setEXP(exp)
-            next
+            puts current
+          else
+            msg = "Please enter a valid selection."
+            puts msg
           end
         when 3
           break
         end
-      else
-        msg = "Please enter the name of an existing character."
+      end
+    end
+
+  def self.getCharacter()
+    roster = get_roster()
+    names = roster.keys()
+    loop do
+      names.each do |x|
+        msg = "\n #{x}"
         puts msg
+      end
+      msg = "\nPlease enter a character name from the list: "
+      puts msg
+      input = gets.chomp
+      if names.include?(input)
+        current = roster[input]
+        return current
+      else
+        msg = "Please enter a valid name."
+        puts msg
+        next
       end
     end
   end
+
 
   def self.menu()
     welcome = 'Welcome to SpeedRPG Characters!'
@@ -276,7 +222,7 @@ class Character
     options = "Please select an option:
     1) Create a New Character
     2) View/Edit an Existing Character
-    3)  Quit"
+    3) Quit"
     bit = false
     loop do
       puts options
@@ -295,11 +241,82 @@ class Character
   c1 = Character.new('Randwulf','High Elf','Paladin','Neutral Good',15,12,10,15,12,14)
   c2 = Character.new('Bobo','Halfling','Rogue','True Neutral',8,15,12,15,12,8)
   c3 = Character.new('Haelan','High Elf','Cleric','Lawful Good',12,12,12,10,15,12)
-  #  @@roster = @@roster.merge!("#{c.name}"=>c)
   menu()
 end
 
 =begin
     @@levelEXP = { _2: 300, _3: 900, _4: 2700, _5: 6500, _6: 14000, _7: 23000, _8: 34000, _9: 48000, _10: 64000, _11: 85000, _12: 100000,
     _13: 120000, _14: 140000, _15: 165000, _16: 195000, _17: 225000, _18: 265000, _19: 305000, _20: 355000 }
+    
+    # if num > @exp
+    #   temp = num - @exp
+    #   addEXP(temp)
+    # elsif num < @exp
+    #   temp = @exp - num
+    #   removeEXP(temp)
+    # end
+
+    # while @exp >= nextLvlEXP()
+    #   addLevels(1)
+    #   break if @level == 20
+    # end
+
+    #  @@roster = @@roster.merge!("#{c.name}"=>c)
+
+    def self.get_levelEXP
+    @@levelEXP
+  end
+
+  def addLevels(num)
+    @level += num
+  end
+
+  def removeLevels(num)
+    @level -= num
+  end
+
+  def nextLvlEXP()
+    if @level == 1
+      0
+    elsif @level == 20
+      355_000
+    else
+      @@levelEXP[:"_#{@level + 1}"]
+    end
+  end
+
+  def prevLvlEXP()
+    if @level == 1
+      0
+    elsif @level == 20
+      355_000
+    else
+      @@levelEXP[:"_#{@level}"]
+    end
+  end
+
+  def addEXP(num)
+    if @level == 20
+      msg = 'You are already at Max Level! (20)'
+      puts msg
+    else
+      @exp += num
+      setLevel(exp)
+      puts self
+    end
+  end
+
+  def removeEXP(num)
+    if @level == 1
+      msg = 'You are already at First Level! (1)'
+      puts msg
+    else
+      @exp -= num
+      while @exp < prevLvlEXP()
+        removeLevels(1)
+        break if @level == 1
+      end
+      puts self
+    end
+  end
 =end
